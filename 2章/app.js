dae8e5f430c2fb866929f7c2636a46d29b91858a
murata <murata.naoya.infratop@gmail.com>
2020-11-17 20:59:15 +0900

    finish[2章]

---
+++ 2章/app.js
@@ -0,0 +1,46 @@
+// var hello = "Hello World";
+// alert(hello);
+
+// var int1 = 1;
+// var int2 = -10;
+// var float1 = 3.14;
+// var str11 = 'Javascriptを覚えよう';
+
+// alert(4 + 3);
+// alert(8 - 5);
+// alert(2 * 6);
+// alert(10 / 2);
+
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
+
+// var orange = 100;
+// var apple = 120;
+
+// if (orange < apple){
+// 	alert('みかんの値段がりんごより安い');
+// }else if (orange == apple){
+// 	alert('みかんとりんごが同じ値段');
+// }else{
+// 	alert('みかんの値段がりんごより高い');
+// }
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+// 	num *= 2
+// 	count++;
+// }
+// alert("2をかけ続けて" + max + "を越えるのに必要だった数は" + count + "回です" );
+
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+	num = num + i;
+}
+
+alert("1から10まで足し算した結果は" + num + "です");
